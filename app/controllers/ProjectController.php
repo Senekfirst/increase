e07231860251d8dfcef6713d0fdfd0a977612b63
Martin <martinchertier@gmail.com>
2015-03-23 15:51:43 +0100

    ajout de la méthode recursivePanel pour pouvoir afficher les messages et les sous-messages avec l'indentation

--- app/controllers/ProjectController.php
+++ app/controllers/ProjectController.php
@@ -10,73 +10,42 @@ class ProjectController extends ControllerBase {
 	{
 	
 	}
+
 	
-	/*public function messagesAction($id) {
-		$bootstrap=$this->jquery->bootstrap();
-		$currentProject = Projet::findFirstByid($id);
-		$messages = $currentProject->getMsgs();
-		
-		$count = 0;
-		$zonesBtn = array();
-		foreach($messages as $message){
-			$bootstrap->htmlButton("btMasquer".$count, $message->getUser()->getIdentite() . ' - ' . $message->getObjet(),"btn-primary",$this->jquery->show("#zoneBoutons".$count));
-			$zonesBtn[$count] = '<div id="#zoneBoutons'. $count .'">'. $message->getContent() ."</div>";
-			$count++;
-		}
-		
-		$this->view->setVars(array('zones' => $zonesBtn));
-		$this->view->disableLevel(View::LEVEL_MAIN_LAYOUT); //Ici on coupe la vue venant du dessus, seule cette partie nous intéresse
-		$this->jquery->compile($this->view);
-	}*/
-	
-	/*public function testMessagesAction($id) {
+	public function messagesAction($id) {
 		$bootstrap=$this->jquery->bootstrap();
 		$currentProject = Projet::findFirstByid($id);
 		$messages = $currentProject->getMsgs();
 		
 		$count = 0;
 		$zonesPanel = array();
+		$retour = "";
 		foreach($messages as $message){
-			$panel = '<div class="panel panel-primary">
-						<div class="panel-heading" onClick="document.getElementById(\'body' . $count . '\').style.display === \'block\'? document.getElementById(\'body' . $count . '\').style.display=\'none\' : document.getElementById(\'body' . $count . '\').style.display=\'block\'">
-							<h3 class="panel-title"> ' . $message->getObjet() . '</h3>
-						</div>
-						<div class="panel-body" style="display:none" id="body' . $count . '">' . $message->getContent() . '</div>
-					  </div>';
-
-			$zonesPanel[$count] = $panel;
-			$count++;
+			if($message->getIdFil() == NULL) {
+				$zonesPanel[$count] = $this->recursivePanel($message, $messages, 0);
+				$count++;
+			}
 		}
 		
 		$this->view->setVars(array('panels' => $zonesPanel));
 		$this->jquery->compile($this->view);
-	}*/
-	
-	// Version avec htmlPanel, mais qui ne fonctionne pas...Attention à modifier la vue en conséquence si on remet cette version du la méthode en prod
-	public function messagesAction($id) {
-	    $bootstrap=$this->jquery->bootstrap();
-	    $currentProject = Projet::findFirstByid($id);
-	    $messages = $currentProject->getMsgs();
-	    
-	    $count = 0;
-	    $zonesPanel = array();
-	    foreach($messages as $message){
-	    	$idPanel = "panel" . $count;
-	        $panel = new HtmlPanel($idPanel);
-	        $panel->setContent($message->getContent());
-	        $panel->setStyle(CssRef::CSS_PRIMARY);
-	        $panel->addHeaderH("<a id='lnk-" . $idPanel . "' href='#collapse-" . $idPanel . "'>" . $message->getObjet() . "</a>","3");
-	        $panel->setCollapsable(false);
-	        $panel->compile($this->jquery, $this->view);
-	        $zonesPanel[$count] = $panel;
-	        $count++;
-	    }
-	
-	    $this->view->setVars(array('panels' => $zonesPanel));
-	    $this->jquery->compile($this->view);
-	    $this->view->disableLevel(View::LEVEL_MAIN_LAYOUT);
+		$this->view->disableLevel(View::LEVEL_MAIN_LAYOUT);
 	}
 	
+	private function recursivePanel ($msg, $arrayMsgs, $offset) {
+		$panel = '<div class="panel panel-primary col-md-offset-' . $offset . '">
+						<div class="panel-heading" onClick="document.getElementById(\'body' . $msg->getId() . '\').style.display === \'block\'? document.getElementById(\'body' .$msg->getId() . '\').style.display=\'none\' : document.getElementById(\'body' . $msg->getId() . '\').style.display=\'block\'">
+							<h3 class="panel-title"> ' . $msg->getObjet() . ' - ' . $msg->getDate() . ' - ' . $msg->getUser()->getIdentite() . '</h3>
+						</div>
+						<div class="panel-body" style="display:none" id="body' . $msg->getId() . '">' . $msg->getContent() . '</br><button class="btn btn-primary">Repondre</button></div>
+					  </div>';
+		foreach($arrayMsgs as $currentMsg) {
+			if($currentMsg->getIdFil() == $msg->getId()) {
+				$panel .= $this->recursivePanel($currentMsg, $arrayMsgs, $offset+1);
+			}
+		}
+		return $panel;
+	}	
 	
 	public function equipeAction($id){
 		$currentProject = Projet::findFirstByid($id);
